63b93f473f3d8eb64c760a915ff54d144c4aa6f4
om <omejjigiri99@gmail.com>
2023-11-28 21:05:12 +0530

    making shell scripting topics - little heading underlining

--- mysql.sh
+++ mysql.sh
@@ -4,11 +4,13 @@ cp mysql.repo /etc/yum.repos.d/mysql.repo
 
 dnf install mysql-community-server -y
 
-mysql_secure_installation --set-root-pass ExpenseApp@1
-
-mysql -uroot -pExpenseApp@1
-
 systemctl enable mysqld
 
 systemctl start mysqld
+
+mysql_secure_installation --set-root-pass ExpenseApp@1
+
+
+
+
 
