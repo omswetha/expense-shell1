46d922e67e16efa49aa2a5b6383e4996377e6f39
om <omejjigiri99@gmail.com>
2023-11-28 17:55:12 +0530

    making shell scripting topics - little heading underlining

---
+++ frontend.sh
@@ -0,0 +1,12 @@
+dnf module disable mysql -y
+
+cp mysql.repo /etc/yum.repos.d/mysql.repo
+
+dnf install mysql-community-server -y
+
+mysql_secure_installation --set-root-pass ExpenseApp@1
+
+mysql -uroot -pExpenseApp@1
+
+systemctl enable mysqld
+systemctl start mysqld
